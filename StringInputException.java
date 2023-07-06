a67d6aecc8a1f57a563a076f5d887140c9bf4e43
YuliyaB <114241910+YuliyaBabenko@users.noreply.github.com>
2023-07-06 15:51:45 +0300

    initial commit

---
+++ StringInputException.java
@@ -0,0 +1,8 @@
+public class StringInputException extends Exception{
+    public StringInputException() {
+    }
+
+    public StringInputException(String message) {
+        super(message);
+    }
+}
